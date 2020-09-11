9d6fc9b031b209c42fad4d30d506d0af61e9191b
leonflixx <ihajileon2017@gmail.com>
2020-09-11 17:43:12 +0300

    first commit

---
+++ src/Components/MyProfile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MyProfile() {
+  return (
+    <div>
+      <h1>Welcome To My Profile Page</h1>
+      <p>
+        I am Leon Ihaji Faheem, a cyber security enthusiast and a front-end web
+        developer. i am obsessed with all things tech-related and i spend my
+        free time building projects using the above programming languages.
+      </p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JAVASCRIPT</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyProfile;
